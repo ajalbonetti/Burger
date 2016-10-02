21638c5df8882f1e649dbf1028d7405847855e28
Aaron A <albon101@Aarons-MacBook-Pro.local>
2016-10-02 11:48:56 -0700

    updated server.js

--- models/burger.js
+++ models/burger.js
@@ -1,13 +1,10 @@
 var orm = require('./config/orm.js');
 
-
 // Select all the data from the burgers table
 orm.selectAll('burgers');
 
-
 // Insert user inputted burger into the database 
 orm.insertOne('burgers', 'burger_name', 'devoured', burgerInput, burgerDevoured);
-
 
 // Updates the value of the devoured column to true
 orm.updateOne('burgers', 'devoured', true, 'burger_name', burgerInput);
